5929e8b3adde076864223d2359289c277957c8a7
Oleg Gordeev <oleg.gordeev@sharp-dev.net>
2024-01-22 13:13:52 +0300

    Enable @typescript-eslint/no-explicit-any

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,9 +20,7 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/require-await': 'error',
     '@typescript-eslint/no-floating-promises': 'error',
-
   },
 };
